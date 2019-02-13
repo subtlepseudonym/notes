77cd0620b5656a7583fdc3bbda1f26b47a2c29c5
Connor Demille <subtlepseudonym@gmail.com>
2019-02-12 21:31:59 -0500

    Add NewDefaultDAL test

--- dal_test.go
+++ dal_test.go
@@ -1,10 +1,36 @@
 package notes
 
 import (
+	"path"
 	"testing"
+
+	"github.com/go-test/deep"
+	"github.com/mitchellh/go-homedir"
 )
 
 func TestNewDefaultDAL(t *testing.T) {
+	version := "totally not a semantic version"
+	dal, err := NewDefaultDAL(version)
+	if err != nil {
+		t.Error(err)
+	}
+
+	home, err := homedir.Dir()
+	if err != nil {
+		t.Error(err)
+		t.FailNow()
+	}
+
+	expected := &defaultDAL{
+		version:            version,
+		metaFilename:       defaultMetaFilename,
+		notesDirectoryPath: path.Join(home, defaultNotesDirectory),
+		noteFilenameFormat: defaultNoteFilenameFormat,
+	}
+
+	if diff := deep.Equal(dal, expected); diff != nil {
+		t.Error(diff)
+	}
 }
 
 func TestDefaultDALGetMeta(t *testing.T) {
