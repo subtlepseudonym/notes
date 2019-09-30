2afdd064d8bda191f45bd78212d0bb8e19e4de7c
Connor Demille <subtlepseudonym@gmail.com>
2019-09-30 16:32:52 -0400

    Replace notes_test with stubs

--- notes_test.go
+++ notes_test.go
@@ -1,36 +1,14 @@
 package notes
 
 import (
-	"io/ioutil"
 	"testing"
 )
 
-type NoteBodyTest struct {
-	EditorCommand string
-	ExistingBody  string
-
-	ExpectedBody string
+func TestMetaApproxSize(t *testing.T) {
 }
 
-func TestGetNoteBodyFromUser(t *testing.T) {
-	tests := []NoteBodyTest{
-		// TODO: add tests for failure cases
-		// TODO: monkey patch os/exec
-	}
+func TestNoteApproxSize(t *testing.T) {
+}
 
-	for _, test := range tests {
-		editFile, err := ioutil.TempFile("", "test")
-		if err != nil {
-			t.Error(err)
-		}
-
-		body, err := GetNoteBodyFromUser(editFile, test.EditorCommand, test.ExistingBody)
-		if err != nil {
-			t.Error(err)
-		}
-
-		if body != test.ExpectedBody {
-			t.Errorf("body %q != expected body %q", body, test.ExpectedBody)
-		}
-	}
+func TestNoteAppendEdit(t *testing.T) {
 }
