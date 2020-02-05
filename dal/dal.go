42aef9924f08aaecc3c2532d0658b3753ad02557
Connor Demille <subtlepseudonym@gmail.com>
2020-02-05 16:51:18 -0500

    Add index functionality to dal

--- dal/dal.go
+++ dal/dal.go
@@ -7,6 +7,9 @@ import (
 // DAL interfaces the method by which we access the source of
 // Meta and Note objects
 type DAL interface {
+	GetIndex() (notes.Index, error)
+	SaveIndex(notes.Index) error
+
 	GetMeta() (*notes.Meta, error)
 	SaveMeta(*notes.Meta) error
 
--- dal/local.go
+++ dal/local.go
@@ -3,8 +3,10 @@ package dal
 import (
 	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
 	"path"
+	"regexp"
 	"sync"
 
 	"github.com/subtlepseudonym/notes"
@@ -13,13 +15,16 @@ import (
 )
 
 const (
+	defaultIndexFilename      = "index"
 	defaultMetaFilename       = "meta"
 	defaultNoteFilenameFormat = "%06d"
+	noteFilenameRegex         = `[0-9]{6}`
 )
 
 type local struct {
 	sync.Mutex
 	version            string
+	indexFilename      string
 	metaFilename       string
 	notesDirectoryPath string
 	noteFilenameFormat string
@@ -34,16 +39,126 @@ func NewLocalDAL(dirName, version string) (DAL, error) {
 
 	return &local{
 		version:            version,
+		indexFilename:      defaultIndexFilename,
 		metaFilename:       defaultMetaFilename,
 		notesDirectoryPath: path.Join(home, dirName),
 		noteFilenameFormat: defaultNoteFilenameFormat,
 	}, nil
 }
 
-func (d *local) buildNewMeta() (*notes.Meta, error) {
+func (d *local) buildNewIndex() (notes.Index, error) {
+	if _, err := os.Stat(d.notesDirectoryPath); os.IsNotExist(err) {
+		err = os.Mkdir(d.notesDirectoryPath, os.ModeDir|os.FileMode(0700))
+		if err != nil {
+			return nil, fmt.Errorf("create notes directory: %v", err)
+		}
+	}
+
+	indexPath := path.Join(d.notesDirectoryPath, d.indexFilename)
+	indexFile, err := os.Create(indexPath)
+	if err != nil {
+		return nil, fmt.Errorf("create index file: %v", err)
+	}
+	defer indexFile.Close()
+
+	index := notes.NewIndex(0) // use default capacity
+	infos, err := ioutil.ReadDir(d.notesDirectoryPath)
+	if err != nil {
+		return nil, fmt.Errorf("read notes directory: %v", err)
+	}
+
+	nameRegex := regexp.MustCompile(noteFilenameRegex)
+	for _, info := range infos {
+		if info.IsDir() || !nameRegex.MatchString(info.Name()) {
+			continue
+		}
+
+		noteFilename := path.Join(d.notesDirectoryPath, info.Name())
+		noteFile, err := os.Open(noteFilename)
+		if err != nil {
+			// TODO: log this
+			continue
+		}
+
+		var note notes.Note
+		err = json.NewDecoder(noteFile).Decode(&note)
+		if err != nil {
+			// TODO: log this
+			continue
+		}
+
+		index[note.Meta.ID] = note.Meta
+	}
+
+	err = json.NewEncoder(indexFile).Encode(index)
+	if err != nil {
+		return nil, fmt.Errorf("encode index file: %v", err)
+	}
+
+	err = indexFile.Close()
+	if err != nil {
+		return index, fmt.Errorf("close index file: %v", err)
+	}
+	return index, nil
+}
+
+func (d *local) GetIndex() (notes.Index, error) {
 	d.Lock()
 	defer d.Unlock()
 
+	indexPath := path.Join(d.notesDirectoryPath, d.indexFilename)
+	indexFile, err := os.Open(indexPath)
+	if err != nil {
+		return d.buildNewIndex()
+	}
+	defer indexFile.Close()
+
+	var index notes.Index
+	err = json.NewDecoder(indexFile).Decode(&index)
+	if err != nil {
+		return nil, fmt.Errorf("decode index file: %v", err)
+	}
+
+	err = indexFile.Close()
+	if err != nil {
+		return index, fmt.Errorf("close index file: %v", err)
+	}
+	return index, nil
+}
+
+func (d *local) SaveIndex(index notes.Index) error {
+	d.Lock()
+	defer d.Unlock()
+
+	indexPath := path.Join(d.notesDirectoryPath, d.indexFilename)
+	err := os.Rename(indexPath, indexPath+".bak")
+	if err != nil {
+		return fmt.Errorf("backup old index file: %v", err)
+	}
+	// TODO: remove index backup
+
+	indexFile, err := os.Create(indexPath)
+	if err != nil {
+		err = os.Rename(indexPath+".bak", indexPath)
+		if err != nil {
+			return fmt.Errorf("restore index backup: %v", err)
+		}
+	}
+	defer indexFile.Close()
+
+	err = json.NewEncoder(indexFile).Encode(index)
+	if err != nil {
+		return fmt.Errorf("encode index file: %v", err)
+	}
+
+	err = indexFile.Close()
+	if err != nil {
+		return fmt.Errorf("close index file: %v", err)
+	}
+	return nil
+}
+
+func (d *local) buildNewMeta() (*notes.Meta, error) {
 	if _, err := os.Stat(d.notesDirectoryPath); os.IsNotExist(err) {
 		err = os.Mkdir(d.notesDirectoryPath, os.ModeDir|os.FileMode(0700))
 		if err != nil {
@@ -60,7 +175,6 @@ func (d *local) buildNewMeta() (*notes.Meta, error) {
 
 	m := &notes.Meta{
 		Version: d.version,
-		Notes:   make(map[int]notes.NoteMeta),
 	}
 
 	err = json.NewEncoder(metaFile).Encode(m)
@@ -83,7 +197,8 @@ func (d *local) GetMeta() (*notes.Meta, error) {
 	metaPath := path.Join(d.notesDirectoryPath, d.metaFilename)
 	metaFile, err := os.Open(metaPath)
 	if err != nil {
-		return d.buildNewMeta()
+		meta, err := d.buildNewMeta()
+		return meta, err
 	}
 	defer metaFile.Close()
 
@@ -141,9 +256,6 @@ func (d *local) getNotePath(id int) string {
 
 // GetNote retrieves and decodes a Note from file
 func (d *local) GetNote(id int) (*notes.Note, error) {
-	d.Lock()
-	defer d.Unlock()
-
 	notePath := d.getNotePath(id)
 	noteFile, err := os.Open(notePath)
 	if err != nil {
