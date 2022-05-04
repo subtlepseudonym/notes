f9ae203a50bdaa59c3617305b29fa6e8e66f08b9
Connor Demille <subtlepseudonym@gmail.com>
2022-05-04 10:59:38 -0400

    Add edit operation function stubs
    Not 100% on what the edit operation will actually do before I get
    started adding features

---
+++ operations/edit.go
@@ -0,0 +1,13 @@
+package operations
+
+import (
+)
+
+func EditNoteBody(dal dal.DAL, id, body string) (*notes.Note, error) {
+}
+
+func EditNoteTitle(dal dal.DAL, id, title string) (*notes.Note, error) {
+}
+
+func EditNoteTags(dal dal.DAL, id string, tags []string) (*notes.Note, error) {
+}
